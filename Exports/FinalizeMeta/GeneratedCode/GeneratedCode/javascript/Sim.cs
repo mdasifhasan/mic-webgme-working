face2b4cbbb23dc5505b2a73cefc556b2a45732c
MD Asif Hasan <md.asif.hasan@vanderbilt.edu>
2016-11-24 12:32:41 -0600

    All the data types under dictionary converted successfully.

--- Exports/FinalizeMeta/GeneratedCode/GeneratedCode/javascript/Sim.cs
+++ Exports/FinalizeMeta/GeneratedCode/GeneratedCode/javascript/Sim.cs
@@ -34,33 +34,34 @@ this.value = value;
 };
 inheritsFrom(Obj, Data);
 
-var ReferData = function (value, dataType) {
+var ReferData = function (referValue, dataType) {
 Data.apply(this);
 this.dataType = dataType;
-this.value = value;
+this.referValue = referValue;
 };
 inheritsFrom(ReferData, Data);
 // end of the primitives
 
 
-
-
-
-
 var DictionarySample = function () {
 Data.apply(this);
-this['0']=new Number(11);
-this['1']=new Number(21);
-this['2']=new Number(31);
+this['0']=new Text('Sample Text');
+this['1']=new Boolean(true);
+this['2']=new Boolean(false);
 this['3']={};
-this['3']['0']=new Number(2);
+this['3']['0']=new Obj(null, 'object');
 this['3']['3']=new Number(4);
-this['3']['4']={};
-this['3']['4']['0']=new Number(2);
+this['3']['4']=new ReferData('null', 'DataTypes.DataSprite');
+this['3']['5']={};
+this['3']['5']['0']=new Number(2);
 this['3']['abs']=new Number(3);
 
 };
 inheritsFrom(DictionarySample , Data);
+
+
+
+
 
 
 
