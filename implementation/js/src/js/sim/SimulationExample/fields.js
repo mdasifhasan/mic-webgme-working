0e4ecd3a4d8076402dd8895b13c39e4c036170f6
MD Asif Hasan <md.asif.hasan@vanderbilt.edu>
2016-11-26 23:01:38 -0600

    Latest

---
+++ implementation/js/src/js/sim/SimulationExample/fields.js
@@ -0,0 +1,21 @@
+/**
+ * Created by AH on 11/24/2016.
+ */
+var Fields = {};
+Fields.fieldCanvas = {};
+Fields.fieldCanvas.fieldSprites = {};
+
+Fields.fieldCanvas.fieldSprites.subscriptions = function () {
+    this["createSprite"] = [];
+    this["createSprite"] = [];
+};
+
+Fields.fieldCanvas.fieldSprites.createSprite = function (ds) {
+    if(!(ds instanceof DataSprite)){
+
+    }
+};
+
+Fields.fieldCanvas.fieldSprites.createGroup = function () {
+
+};
