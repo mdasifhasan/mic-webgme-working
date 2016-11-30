825e4c8bb13e7abb00ddf1c0e361661ca7570a35
MD Asif Hasan <md.asif.hasan@vanderbilt.edu>
2016-11-30 14:04:36 -0600

    Agent DataStructure done, without formatting.

--- Exports/CodeGen3/GeneratedCode/GeneratedCode/javascript/agents.cs
+++ Exports/CodeGen3/GeneratedCode/GeneratedCode/javascript/agents.cs
@@ -4,6 +4,7 @@ var Canvas = function (name) {
     if(!name)
         name = "Canvas";
     Agent.apply(this, [name]);
+    
 };
 inheritsFrom(Canvas, Agent);
 Canvas.library = {};
@@ -11,29 +12,53 @@ Canvas.library.Text = function (name) {
     if(!name)
         name = "Text";
     Agent.apply(this, [name]);
+    
+    this.data = {};
+    this.data['DataText'] = new DataText();
+    
+
+
 };
 inheritsFrom(Canvas.library.Text, Agent);
 Canvas.library.Sprite = function (name) {
     if(!name)
         name = "Sprite";
     Agent.apply(this, [name]);
+    
+    this.data = {};
+    this.data['DataSprite'] = new DataSprite();
+    
+
+
 };
 inheritsFrom(Canvas.library.Sprite, Agent);
 var GameEngine = function (name) {
     if(!name)
         name = "GameEngine";
     Agent.apply(this, [name]);
+    
 };
 inheritsFrom(GameEngine, Agent);
 var TestGame = function (name) {
     if(!name)
         name = "TestGame";
     Agent.apply(this, [name]);
+    
     var child = null;
     var child = new TestGame.library.Star('Star2');
     this.addChild(child);
+    
+
+
     var child = new TestGame.library.Star('Star1');
     this.addChild(child);
+    child.childs['Sprite'].data['DataSprite']['y'].value = 100;
+child.childs['Sprite'].data['DataSprite']['x'].value = 100;
+
+child.childs['Sprite'].data['DataSprite']['Group']['enable physics'].value = true;
+
+
+
 };
 inheritsFrom(TestGame, Agent);
 TestGame.library = {};
@@ -41,9 +66,13 @@ TestGame.library.Star = function (name) {
     if(!name)
         name = "Star";
     Agent.apply(this, [name]);
+    
     var child = null;
     var child = new Canvas.library.Sprite('Sprite');
     this.addChild(child);
+    
+
+
 };
 inheritsFrom(TestGame.library.Star, Agent);
 TestGame.library.Star.library = {};
@@ -51,6 +80,7 @@ TestGame.library.Star.library.SampleAgent = function (name) {
     if(!name)
         name = "SampleAgent";
     Agent.apply(this, [name]);
+    
 };
 inheritsFrom(TestGame.library.Star.library.SampleAgent, Agent);
 TestGame.library.Star.library.SampleAgent.library = {};
@@ -58,25 +88,38 @@ TestGame.library.Star.library.SampleAgent.library.SampleAgent2 = function (name)
     if(!name)
         name = "SampleAgent2";
     Agent.apply(this, [name]);
+    
 };
 inheritsFrom(TestGame.library.Star.library.SampleAgent.library.SampleAgent2, Agent);
 var ErrorHandler = function (name) {
     if(!name)
         name = "ErrorHandler";
     Agent.apply(this, [name]);
+    
 };
 inheritsFrom(ErrorHandler, Agent);
 var Debug = function (name) {
     if(!name)
         name = "Debug";
     Agent.apply(this, [name]);
+    
     var child = null;
     var child = new Canvas.library.Text('Text');
     this.addChild(child);
+    
+
+
 };
 inheritsFrom(Debug, Agent);
 
 // end of generated code
+
+
+
+
+
+
+
 
 
 
